a82f0cd4d9c8bfdb012968664c808dc9bb9c53ac
Ghazi Triki <ghazi.triki@riadvice.com>
2026-09-01 18:59:26 +0100

    Update github actions versions.

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GO111MODULE="on"
-go test -tags 'watch' -v -race -coverprofile=coverage.out -covermode=atomic -coverpkg github.com/WebSummoner/ggr
+go test -tags 'watch' -v -race -coverprofile=coverage.out -covermode=atomic -coverpkg github.com/websummoner/ggr
 cp coverage.out coverage.txt
 
 go install golang.org/x/vuln/cmd/govulncheck@latest
